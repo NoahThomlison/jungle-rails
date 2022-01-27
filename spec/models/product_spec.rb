9a95c84863ed33f3a71cdd8ca9c407f95dfaa9c6
noahthomlison <noahthomlison@gmail.com>
2022-01-26 21:35:11 -0800

    refactors product.new to take in paramaters instead of declaring them after

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,11 +5,7 @@ RSpec.describe Product, type: :model do
     # validation tests/examples here
     it 'should be able to save when given required fields' do
       @category = Category.new
-      @product = Product.new
-      @product.category = @category
-      @product.name =  'Men\'s Classy shirt'
-      @product.price = 1
-      @product.quantity = 1
+      @product = Product.new(category: @category, name: 'Men\'s Classy shirt', price: 1, quantity: 1)
       @product.save!
       expect(@product.category).to be_present
       expect(@product.name).to be_present
@@ -17,42 +13,26 @@ RSpec.describe Product, type: :model do
       expect(@product.quantity).to be_present
     end
     it "should throw an error if name is blank" do
-      @product = Product.new
       @category = Category.new
-      @product.category = @category
-      @product.name =  nil
-      @product.price = 1
-      @product.quantity = 1
+      @product = Product.new(category: @category, name: nil, price: 1, quantity: 1)
       @product.validate
       expect(@product.errors.full_messages).to eq(["Name can't be blank"])
     end
 
     it "should throw an error if category is blank" do
-      @product = Product.new
-      @product.category = nil
-      @product.name =  'Men\'s Classy shirt'
-      @product.price = 1
-      @product.quantity = 1
+      @product = Product.new(category: nil, name: 'Men\'s Classy shirt', price: 1, quantity: 1)
       @product.validate
       expect(@product.errors.full_messages).to eq(["Category can't be blank"])
     end
     it "should throw an error if price is blank" do
-      @product = Product.new
       @category = Category.new
-      @product.category = @category
-      @product.name =  'Men\'s Classy shirt'
-      @product.price = nil
-      @product.quantity = 1
+      @product = Product.new(category: @category, name: 'Men\'s Classy shirt', price: nil, quantity: 1)
       @product.validate
       expect(@product.errors.full_messages).to eq(["Price cents is not a number", "Price is not a number", "Price can't be blank"])
     end
     it "should throw an error if quantity is blank" do
-      @product = Product.new
       @category = Category.new
-      @product.category = @category
-      @product.name =  'Men\'s Classy shirt'
-      @product.price = 1
-      @product.quantity = nil
+      @product = Product.new(category: @category, name: 'Men\'s Classy shirt', price: 1, quantity: nil)
       @product.validate
       expect(@product.errors.full_messages).to eq(["Quantity can't be blank"])
     end
