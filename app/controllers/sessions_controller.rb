487df3622d57ad7fa8fa968dfa54dbbb800d4bed
noahthomlison <noahthomlison@gmail.com>
2022-01-27 20:45:19 -0800

    refactors create method to call new authenticate_with_credentials method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,24 +5,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user 
-      # logged in when they navigate around our website.
+    if user = User.authenticate_with_credentials(params[:email], param[:password])
+      puts user.id
       session[:user_id] = user.id
       redirect_to '/'
     else
-    # If user's login doesn't work, send them back to the login form.
+      puts "redirected"
       redirect_to '/login'
-    end
+    end    
   end
-
 
   def destroy
     session[:user_id] = nil
     redirect_to '/login'
   end
-
-
 end
