e105f786343b020ae0de3b56ead81f70b2e5bb11
noahthomlison <noahthomlison@gmail.com>
2022-01-27 20:46:09 -0800

    refactors user class to have autentical_with_credentials and properly return user if they enter valid credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,18 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, confirmation: true, length: { minimum: 8 }
   validates :password_confirmation, presence: true
-
   has_secure_password
 
   def authenticate_with_credentials(email, password)
-    @user = User.new
-  end
+    email = email.strip
+    email = email.downcase
+    user = User.find_by_email(email)
 
+    # If the user exists AND the password entered is correct.
+    if user && user.authenticate(password)
+      return user
+    else
+      nil
+    end
+  end
 end
