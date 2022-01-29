4c6aeeac9081b43bc8aeee1e01cae554fff281d9
noahthomlison <noahthomlison@gmail.com>
2022-01-29 11:47:54 -0800

    refactors to insatnce user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,16 @@
 class User < ActiveRecord::Base
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, confirmation: true, length: { minimum: 8 }
   validates :password_confirmation, presence: true
   has_secure_password
 
-  def authenticate_with_credentials(email, password)
+  def self.authenticate_with_credentials(email, password)   
     email = email.strip
-    email = email.downcase
-    user = User.find_by_email(email)
-
-    # If the user exists AND the password entered is correct.
-    if user && user.authenticate(password)
-      return user
+    email = email.downcase    
+    @user = User.find_by_email(email)
+    if @user && @user.authenticate(password)
+      @user
     else
       nil
     end
