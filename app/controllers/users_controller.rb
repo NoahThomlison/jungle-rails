347ec4c8b0c6cdf61597cb26e35ca87782560fe0
noahthomlison <noahthomlison@gmail.com>
2022-01-29 11:47:28 -0800

    refactors to instance users and removes last_name first_name and replaces with name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,20 +3,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to '/'
-    else
-      redirect_to '/signup'
-    end
+    @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to '/'
+      else
+        redirect_to '/signup'
+      end
   end
 
   private
   def user_params
     params.require(:user).permit(
-      :first_name, 
-      :last_name, 
+      :name, 
       :email, 
       :password, 
       :password_confirmation
