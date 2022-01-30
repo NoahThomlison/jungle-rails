934f3a61e48407ad22407c3e6ede364a6ad3058d
noahthomlison <noahthomlison@gmail.com>
2022-01-30 14:33:35 -0800

    adds downcase before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ActiveRecord::Base
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: true, case_sensitive: false 
   validates :password, presence: true, confirmation: true, length: { minimum: 8 }
   validates :password_confirmation, presence: true
   has_secure_password
+  before_save { email.downcase!}
 
-  def self.authenticate_with_credentials(email, password)   
+  def authenticate_with_credentials(email, password)   
     email = email.strip
     email = email.downcase    
+    puts email
     @user = User.find_by_email(email)
     if @user && @user.authenticate(password)
       @user
